4b60f975b0cd68201c7326d8ee69ce06e45274db
VarianWrynn <yuna1111@163.com>
2020-04-21 13:02:38 +0800

    debugger for music

--- components/classic/music/index.js
+++ components/classic/music/index.js
@@ -17,16 +17,20 @@ Component({
   },
 
   lifetimes: {
-    attached: function () {
+    attached: function (event) {
       // 在组件实例进入页面节点树时执行
-      // this.method._recoverStatus();这种调用方式会报错
+      //  this.method._recoverStatus();这种调用方式会报错
+      //this.methods._recoverStatus();
+
+      console.log("exe attached");
       this._recoverStatus();
     },
-    detached: function () {
+    detached: function (event) {
       // 在组件实例被从页面节点树移除时执行
 
       /*设置当前页面消失的时候停止播放音乐,但是由于父页面使用是hidden属性来隐藏当前组件，因此是不会触发detached函数，但是如果设置为wx:if是可以触发该函数*/
       //mMgr.stop()
+      console.log("exe detached");
     },
   },
 
@@ -44,32 +48,41 @@ Component({
    */
   methods: {
     onPlay: function (event) {
+      //console.log("exeting onPlay");
       if (!this.data.playing) {
         //首先切换图片为pause
+        //console.log("-- exeting onPlay set playing is true;");
         this.setData({
           playing: true
         });
         mMgr.title = "Lee's playing"; //必填
         mMgr.src = "https://www.tmclee.com/Lee/lee.m4a";
       } else {
+        //console.log("-- exeting onPlay set playing is flase and pause music;");
         this.setData({
           playing: false
         });
         mMgr.pause();
       }
+    },
+
+    _recoverStatus: function () {
+      console.log("exe _recoverStatus")
+      if(mMgr.pause){ //如果当前没有背景音乐
+        this.setData({
+          playing:false
+        })
+       // return; //设置完则直接返回不执行后续代码，否则可能会执行后续的代码
+      }
+      console.log("mMgr.src= "+mMgr.src)
+      console.log("properties.src= "+this.properties.src)
+      if(mMgr.src == this.properties.src){ //当前播放的音乐就是Music组件所展示的音乐
+      
+        this.setData({
+          playing:true
+        })
+      }
     }
-  },
-  _recoverStatus: function () {
-    if(mMgr.pause){ //如果当前没有背景音乐
-      this.setData({
-        playing:false
-      })
-      return; //设置完则直接返回不执行后续代码，否则可能会执行后续的代码
-    }
-    if(mMgr.src == this.properties.src){ //当前播放的音乐就是Music组件所展示的音乐
-      this.setData({
-        playing:true
-      })
-    }
+
   }
 })
